2173f92b35fa4bcfb15b55040ad6f279012cfdc5
Tanaka Hayate <pinqphayate@gmail.com>
2016-12-21 16:42:55 +0900

    add script to create model

--- parser_wiki.py
+++ parser_wiki.py
@@ -1,6 +1,9 @@
 #coding:utf-8
 import re, os
 import MeCab
+import word2vec
+
+FILE_NAME = 'out.txt'
 
 def parser_wiki(filename):
     m = MeCab.Tagger ("mecabrc")
@@ -46,9 +49,10 @@ def find_all_files(directory):
 dir = '/Users/kishi-lab/mogami/coupus/extracted/AC'
 # get list of all file
 list = find_all_files(dir)
-f = open('coupus.txt','a')
+f = open( FILE_NAME, 'a')
 for filename in list:
     if re.match('^wiki_', filename):
         words = parser_wiki(filename)
         f.write(words)
 f.close()
+word2vec.create_model( FILE_NAME )
